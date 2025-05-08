97afdc2bac8a072e3ead982d02939800f9470f0b
Matias Aguilera <matias.aguilera@alma.cl>
2025-05-08 12:34:00 +0200

    blank

--- cppTelescope/include/TelescopeImpl.h
+++ cppTelescope/include/TelescopeImpl.h
@@ -1,6 +1,6 @@
-#ifndef _HELLO_COMPONENT_IMPL_H
-#define _HELLO_COMPONENT_IMPL_H
- 
+#ifndef _TELESCOPE_IMPL_H
+#define _TELESCOPE_IMPL_H
+
 #ifndef __cplusplus
 #error This is a C++ include file and cannot be used from plain C
 #endif
@@ -12,7 +12,7 @@
 #include <TelescopeS.h>
  
 //Error definitions for catching and raising exceptions
-class TelescopeImpl : public virtual acscomponent::ACSComponentImpl, public virtual POA_workshop::Telescope {
+class TelescopeImpl : public virtual acscomponent::ACSComponentImpl, public virtual POA_TELESCOPE_MODULE::Telescope {
   public:
     TelescopeImpl(const ACE_CString& name, maci::ContainerServices * containerServices);
     virtual ~TelescopeImpl();
