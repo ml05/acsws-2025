9c6e6a82199e1edb261fb6f9b1e6a29da7ba437f
Matias Aguilera <matias.aguilera@alma.cl>
2025-05-09 10:49:33 +0200

    Revert "include error handling and logging"
    This reverts commit 91c33aef9feb2a941d1b32ee6947505be79af6b0.

--- cppTelescope/src/TelescopeImpl.cpp
+++ cppTelescope/src/TelescopeImpl.cpp
@@ -1,8 +1,7 @@
 #include <TelescopeImpl.h>
 #include <TelescopeControlC.h>
 #include <InstrumentC.h>
-#include <SYSTEMErr.h>
-
+ 
 TelescopeImpl::TelescopeImpl(const ACE_CString& name, maci::ContainerServices * containerServices) : ACSComponentImpl(name, containerServices) {
 
 	pos.az = 0.;
@@ -17,11 +16,9 @@ char* TelescopeImpl::printHello() {
     std::cout << "Just printing 'Hello World!'" << std::endl;
     return CORBA::string_dup("Hello World!");
 }
-// PositionOutOfLimitsEx
+
 void TelescopeImpl::moveTo(TYPES::Position const& coordinates) {
-    if (coordinates.az < 0 || coordinates.az > 360 || coordinates.el > 90 || coordinates.el < 0) {
-        throw SYSTEMErr::PositionOutOfLimitsEx(__FILE__, __LINE__, "Coordinates out of limits").getAlreadyInAutomaticEx();
-    }
+
     // Request the TelescopeControl Component
     TELESCOPE_MODULE::TelescopeControl *comp = this->getContainerServices()->getComponent<TELESCOPE_MODULE::TelescopeControl>("TELESCOPE_CONTROL");
 
@@ -29,24 +26,20 @@ void TelescopeImpl::moveTo(TYPES::Position const& coordinates) {
 
     // Release Component
     this->getContainerServices()->releaseComponent(comp->name());
-    ACS_SHORT_LOG((LM_INFO, "All done. Final position: (%d,%d)", coordinates.el, coordinates.az));
+
 }
 
 TYPES::ImageType* TelescopeImpl::observe(const TYPES::Position& coordinates, CORBA::Long exposureTime) {
-    try {
-        moveTo(coordinates);
-        // Request the Instrument Component
-        INSTRUMENT_MODULE::Instrument *comp = this->getContainerServices()->getComponent<INSTRUMENT_MODULE::Instrument>("INSTRUMENT_S");
-        TYPES::ImageType* result = comp->takeImage(exposureTime);
-        // Release Component
-        this->getContainerServices()->releaseComponent(comp->name());
-    } catch(SYSTEMErr::PositionOutOfLimitsEx) {
-        ACS_SHORT_LOG((LM_ERROR, "Coordinates out of limits"));
-        throw SYSTEMErr::PositionOutOfLimitsEx(__FILE__, __LINE__, "Coordinates out of limits").getAlreadyInAutomaticEx();
-    } catch(SYSTEMErr::CameraIsOffEx) {
-        ACS_SHORT_LOG((LM_ERROR, "Error taking the picture: Camera is off"));
-        throw SYSTEMErr::PositionOutOfLimitsEx(__FILE__, __LINE__, "Camera is off").getAlreadyInAutomaticEx();
-    }
+
+    moveTo(coordinates);
+    
+    // Request the Instrument Component
+    INSTRUMENT_MODULE::Instrument *comp = this->getContainerServices()->getComponent<INSTRUMENT_MODULE::Instrument>("INSTRUMENT_S");
+
+    TYPES::ImageType* result = comp->takeImage(exposureTime);
+
+    // Release Component
+    this->getContainerServices()->releaseComponent(comp->name());
 
     return result;
 
